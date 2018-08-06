f5f1a213c7df8f8150a67f327d5976b6ceba6a48
Andreas Georgiou <ageorgiou@lon-sp-86fd57.home>
2018-08-06 12:18:07 +0100

    Moved to Python3

--- hosthunter.py
+++ hosthunter.py
@@ -12,11 +12,11 @@
 # Version: v1.0
 #
 #
-# Usage Example:
+# [+] Usage Example:
 #
-# $ python hosthunter.py <targets.txt>
+#       $ python3 hosthunter.py <targets.txt>
 #
-# $ cat vhosts.csv
+#       $ cat vhosts.csv
 #
 
 import argparse
@@ -26,13 +26,14 @@ import socket
 import time
 import requests
 import OpenSSL
-import urllib2
+import urllib
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+version="v1.0"
 path = sys.argv[1]
-targets = open(path,"r")
-vhostsf = open("vhosts.csv", "wb")
+targets = open(path,"rt")
+vhostsf = open("vhosts.csv", "wt")
 vhostsf.write("IP Address,Port/Protocol,Domains,Operating System,OS Version,Notes\n") #vhosts.csv Header
 counter=0
 context = ssl.create_default_context()
@@ -41,25 +42,34 @@ context.verify_mode = ssl.CERT_OPTIONAL
 context.load_default_certs()
 
 start_time = time.time()
-print "\n HostHunter v1.0"
-print " Author: ageorgiou@trustwave.com\n"
-version='v1.0'
+banner=(
+    " | $$  | $$                      | $$    | $$  | $$                      | $$\n"
+    " | $$  | $$  /$$$$$$   /$$$$$$$ /$$$$$$  | $$  | $$ /$$   /$$ /$$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$\n"
+    " | $$$$$$$$ /$$__  $$ /$$_____/|_  $$_/  | $$$$$$$$| $$  | $$| $$__  $$|_  $$_/   /$$__  $$ /$$__  $$\n"
+    " | $$__  $$| $$  \ $$|  $$$$$$   | $$    | $$__  $$| $$  | $$| $$  \ $$  | $$    | $$$$$$$$| $$  \__/\n"
+    " | $$  | $$| $$  | $$ \____  $$  | $$ /$$| $$  | $$| $$  | $$| $$  | $$  | $$ /$$| $$_____/| $$\n"
+    " | $$  | $$|  $$$$$$/ /$$$$$$$/  |  $$$$/| $$  | $$|  $$$$$$/| $$  | $$  |  $$$$/|  $$$$$$$| $$\n"
+    " |__/  |__/ \______/ |_______/    \___/  |__/  |__/ \______/ |__/  |__/   \___/   \_______/|__/  " + version + "\n"
+)
 
+print ("%s" % banner)
+print ("\n" ,"HostHunter", version)
+print (" Author: ageorgiou@trustwave.com\n")
 # Read targets.txt file
 for ip in targets:
     ip=ip.replace("\n","")
-    print "\n[+] Target: %s" % ip
+    print ("\n[+] Target: %s" % ip)
     hostnames=''
 # Querying HackerTarget.com API
     try:
-        r2 = urllib2.urlopen('https://api.hackertarget.com/reverseiplookup/?q=%s' % ip).read()
-
+        r2 = urllib.request.urlopen('https://api.hackertarget.com/reverseiplookup/?q=%s' % ip).read().decode("UTF-8")
+    #    print ("Here %s" % r2)
         if (r2.find("No DNS A records found")==-1) and (r2.find("API count exceeded")==-1):
             hostnames=r2
         else:
             hostnames=''
-    except urllib2.HTTPError as e:
-        print "[*] Error connecting with HackerTarget.com API"
+    except urllib.error.HTTPError as e:
+        print ("[*] Error connecting with HackerTarget.com API")
 
 # Fetch SSL certificate
     try:
@@ -73,12 +83,12 @@ for ip in targets:
         pass
 
     if hostnames != "":
-        print "[+] Hostnames: \n%s" % hostnames
+        print ("[+] Hostnames: \n%s" % hostnames)
         row = "\"" + ip + "\"," + "\"443/tcp\"" + "," + "\"" + hostnames.replace("\n",",") + "\""  + "\n"
-        counter += 1
+        counter += hostnames.count('\n')
         vhostsf.write(row)
     else:
-        print "[-] Hostnames: no results "
+        print ("[-] Hostnames: no results ")
         continue
 
 # END IF
@@ -87,6 +97,6 @@ targets.close()
 #  Robtex
 #  https://freeapi.robtex.com/pdns/reverse/193.56.46.229
 
-
-print "\n\n Reconnaissance Completed!\n"
-print "\n %s hostnames were discovered in %s sec\n\n" % (counter,round(time.time() - start_time,2))
+print ("-" * 40)
+print ("\n\n Reconnaissance Completed!\n")
+print ("\n %s hostnames were discovered in %s sec\n\n" % (counter,round(time.time() - start_time,2)))
