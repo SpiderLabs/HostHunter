76c0c894c4b8920190740bbb2bb11653d79fb598
Andreas Georgiou <ageorgiou@trustwave.com>
2018-10-27 13:46:54 +0100

    Initial Commit

--- hosthunter.py
+++ hosthunter.py
@@ -8,20 +8,21 @@
 #    | $$  | $$|  $$$$$$/ /$$$$$$$/  |  $$$$/| $$  | $$|  $$$$$$/| $$  | $$  |  $$$$/|  $$$$$$$| $$
 #    |__/  |__/ \______/ |_______/    \___/  |__/  |__/ \______/ |__/  |__/   \___/   \_______/|__/1
 #
-# Author : Andreas Georgiou (superhedgy)
-# Email  : ageorgiou@trustwave.com
+# Author  : Andreas Georgiou (superhedgy)
+# Email   : ageorgiou@trustwave.com
+# Twitter : @Mr_AndreasGeo
 # Version: v1.5
 #
-#
-# [+] Usage Example:
+# [+] Simple Usage Example:
 #
 #       $ python3 hosthunter.py <targets.txt>
 #
 #       $ cat vhosts.csv
-#
 
+# Standard Libraries
 import argparse
 import sys
+import os
 import ssl
 import socket
 import time
@@ -29,33 +30,52 @@ import requests
 import OpenSSL
 import urllib
 import re
+
+# External Libraries
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # Constants
-version="1.5"
-last_update= "27/10/2018"
-author= "Andreas Georgiou (superhedgy)"
-these_regex="<cite>(.+?)</cite>"
-counter=0
+_name="HostHunter"
+_version="1.5"
+_author="Andreas Georiou"
+_last_update= "20/11/2018"
+_author= "Andreas Georgiou (superhedgy)"
+
+# Options
+chrome_opt = Options()
+#chrome_opt.add_argument("--headless")
+DRIVER = 'chromedriver'
+sc_path = 'screen_captures'
 context = ssl.create_default_context()
 context.check_hostname = False
 context.verify_mode = ssl.CERT_OPTIONAL
 context.load_default_certs()
-regx = "<cite>(.+?)</cite>"
+regx = "<li class=\"b_algo\"(.+?)</li>"
+regx_h3 = "<h2><a hre=\"(.?)\""
+regx_v4 = "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}"
+regx_v6 = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
+pattern_v4 = re.compile(regx_v4)
+pattern_v6 = re.compile(regx_v6)
 pattern=re.compile(regx)
+# Hack to make things faster
+socket.setdefaulttimeout(5)
+
 
 ## Argument Parser
 parser = argparse.ArgumentParser(
-description='< HostHunter Help Page >',
-epilog="Author: ageorgiou@trustwave.com"
+description='|<--- HostHunter Help Page --->|',
+epilog="Author: " + _author
 )
-## nargs='?' to capture mutliple variables
+
 parser.add_argument("-V","--version",help="Displays the currenct version.",action="store_true",default=False)
 parser.add_argument("targets",help="Sets the path of the target IPs file." , type=str, default="targets.txt")
 parser.add_argument("-f","--format",help="Choose between CSV and TXT output file formats.", default="csv")
 parser.add_argument("-o","--output", help="Sets the path of the output file.", type=str,default="vhosts.csv")
 parser.add_argument("-b","--bing",help="Use Bing.com search engine to discover more hostnames associated with the target IP addreses.",action="store_true",default=False)
+parser.add_argument("-sc","--screen-capture",help="Capture a screen shot of any associated Web Applications.",action="store_true",default=False)
 args = parser.parse_args()
 
 if args.format.lower() != "txt" and args.format.lower() != "csv":
@@ -65,119 +85,205 @@ if args.format.lower() != "txt" and args.format.lower() != "csv":
 
 if args.version:
     print("HostHunter version",version)
-    print("Last Updated:",last_update)
-    print("Author:", author)
+    print("Last Updated:",_last_update)
+    print("Author:", _author)
     exit()
 
 # Targets Input File
-targets = open(args.targets,"rt")
-# # Write to a .CSV file
-vhostsf = open(args.output, "wt")
+if not os.path.exists(args.targets):
+    print("\n[*] Error: targets file",args.targets,"does not exist.\n")
+    exit()
+
+targets = open(args.targets,"rt") # Read File
+vhostsf = open(args.output, "wt") # Write File
+appsf = open("webapps.txt", "wt") # Write File
 
 if args.format.lower() == "csv":
     vhostsf.write("\"" + "IP Address" + "\",\"" + "Port/Protocol" + "\",\"" + "Domains" +  "\",\""
     + "Operating System" + "\",\"" + "OS Version" + "\",\"" + "Notes" +  "\"\n") #vhosts.csv Header
 
-banner=(
-    " | $$  | $$                      | $$    | $$  | $$                      | $$\n"
-    " | $$  | $$  /$$$$$$   /$$$$$$$ /$$$$$$  | $$  | $$ /$$   /$$ /$$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$\n"
-    " | $$$$$$$$ /$$__  $$ /$$_____/|_  $$_/  | $$$$$$$$| $$  | $$| $$__  $$|_  $$_/   /$$__  $$ /$$__  $$\n"
-    " | $$__  $$| $$  \ $$|  $$$$$$   | $$    | $$__  $$| $$  | $$| $$  \ $$  | $$    | $$$$$$$$| $$  \__/\n"
-    " | $$  | $$| $$  | $$ \____  $$  | $$ /$$| $$  | $$| $$  | $$| $$  | $$  | $$ /$$| $$_____/| $$\n"
-    " | $$  | $$|  $$$$$$/ /$$$$$$$/  |  $$$$/| $$  | $$|  $$$$$$/| $$  | $$  |  $$$$/|  $$$$$$$| $$\n"
-    " |__/  |__/ \______/ |_______/    \___/  |__/  |__/ \______/ |__/  |__/   \___/   \_______/|__/  " + version + "\n"
-)
+def display_banner():
+    banner=(
+        "                                                                             \n"
+        " | $$  | $$                      | $$    | $$  | $$                      | $$\n"
+        " | $$  | $$  /$$$$$$   /$$$$$$$ /$$$$$$  | $$  | $$ /$$   /$$ /$$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$\n"
+        " | $$$$$$$$ /$$__  $$ /$$_____/|_  $$_/  | $$$$$$$$| $$  | $$| $$__  $$|_  $$_/   /$$__  $$ /$$__  $$\n"
+        " | $$__  $$| $$  \ $$|  $$$$$$   | $$    | $$__  $$| $$  | $$| $$  \ $$  | $$    | $$$$$$$$| $$  \__/\n"
+        " | $$  | $$| $$  | $$ \____  $$  | $$ /$$| $$  | $$| $$  | $$| $$  | $$  | $$ /$$| $$_____/| $$\n"
+        " | $$  | $$|  $$$$$$/ /$$$$$$$/  |  $$$$/| $$  | $$|  $$$$$$/| $$  | $$  |  $$$$/|  $$$$$$$| $$\n"
+        " |__/  |__/ \______/ |_______/    \___/  |__/  |__/ \______/ |__/  |__/   \___/   \_______/|__/  " + _version + "\n"
+    )
 
-print ("%s" % banner)
-print ("\n" ,"HostHunter: ", version)
-print (" Author: ageorgiou@trustwave.com\n")
-start_time = time.time()
-# Read targets.txt file
-for ip in targets:
-    hname = [] #Decleare List
-    ip=ip.replace("\n","")
-    print ("\n[+] Target: %s" % ip)
-    hostnames=''
+    print ("%s" % banner)
+    print ("\n" ,"HostHunter: ", _version)
+    print (" Author : ageorgiou@trustwave.com")
+    print (" Twitter: @mr_andreasgeo")
+    print ("\n" + "|" + "-" * 95 + "|", end = "\n")
 
-    # Querying HackerTarget.com API
+class target:
+    def __init__(self,address):
+        self.address = address
+        self.hname = []
+        self.apps = []
+        self.ipv6 = False
+
+def take_screenshot(IP,port):
+    if port == 443:
+        url="https://" + IP
+    elif (port == 80 or port == 8080):
+        url="http://" + IP
+
+    #print ("Printing the SC URL",url)
     try:
-        r2 = urllib.request.urlopen('https://api.hackertarget.com/reverseiplookup/?q=%s' % ip).read().decode("UTF-8")
-        if (r2.find("No DNS A records found")==-1) and (r2.find("API count exceeded")==-1):
-            for host in r2.split('\n'):
-                if (host=="") or (host in hname):
-                    pass
-                else:
-                    hname.append(host)
-        else:
-            pass
-    except urllib.error.HTTPError as e:
-        print ("[*] Error connecting with HackerTarget.com API")
-
-
-    # Querying Bing.com
-    if args.bing == True:
-        try:
-            r3 = urllib.request.urlopen('https://www.bing.com/search?q=ip%%3a%s' % ip).read().decode("UTF-8")
-            bing_results = re.findall(pattern,r3)
-            for item in bing_results:
-                item2 = re.sub("<.[a-z]+>","",item)
-                host  = re.sub("http[s]://","",item2)
-                if "\/" in host:
-                    print ('TEST')
-                    print(host)
-                if (host=="") or (host in hname):
-                    pass
-                else:
-                    hname.append(host)
-        except urllib.error.HTTPError as e:
-            print ("[*] Error accessing Bing.com")
-
-# Fetch SSL certificate
-    try:
-        # Hack to make things faster
-        r = requests.get('https://%s' % ip, timeout=4,verify=False)
-        cert=ssl.get_server_certificate((ip, 443))
-        x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
-        cert_hostname=x509.get_subject().CN
-
-        #### Add Items to List ####
-        for host in cert_hostname.split('\n'):
-            if (host=="") or (host in hname):
-                pass
-            else:
-                hname.append(host)
-    except (requests.ConnectionError,requests.Timeout,socket.error) as e:
+        driver.implicitly_wait(5)
+        driver.get(url)
+        driver.get_screenshot_as_file(sc_path+"/"+IP+"_443.png")
+    except:
         pass
+        #print("timeout")
 
-    if hname:
-        print ("[+] Hostnames: ", end = "\n")
-        for item in hname:
-            print (item)
-            vhostsf.write(item + "\n")
-            counter += 1
+def brute_force():
+    print("Brute Force")
+    # https://findsubdomains.com/top-subdomain-name-count
+    # https://github.com/rbsec/dnscan/blob/master/subdomains-100.txt
+    # https://www.virustotal.com/en/domain/andreasgeo.com/information/
 
-        if args.format.lower() == 'csv':
-            # Write output to .CSV file
-            hostnames = ','.join(hname) # Merging the lists prooved Faster than list iterations
-            row = "\"" + ip + "\"," + "\"443/tcp\"" + "," + "\"" + hostnames + "\",\"\",\"\",\"\"" + "\n"
-            vhostsf.write(row)
-
+def validate(targ):
+    #print(IP)
+    if not bool(re.match(pattern_v4,targ.address)):
+        if bool(re.match(pattern_v6,targ.address)) == True:
+            targ.ipv6 = True
+        else:
+            print ("\n\"",targ.address,"\" is not a valid IPv4 or IPv6 address.")
+            return False
     else:
-        print ("[-] Hostnames: no results ")
-        continue
+        True
 
-# END IF
-targets.close()
+# Main Function - Read IPs from <targets.txt> file
+def main(argc):
+    counter=0
 
-# IPV6 https://[%ip6]
-#  Robtex
-#  https://freeapi.robtex.com/pdns/reverse/$ip
+    for ip in targets:
+        ip=ip.replace("\n","")
+        hostx = target(ip)
+        if validate(hostx) == False:
+            continue
 
-print ("\n" + "|" + "-" * 65 + "|", end = "\n\n")
-print ("  Reconnaissance Completed!", end = "\n\n")
-if counter==0:
-    print ("  1 hostname was discovered in %s sec" % (round(time.time() - start_time,2)), end = "\n\n")
-else:
-    print ("  %s hostnames were discovered in %s sec" % (counter,round(time.time() - start_time,2)), end = "\n\n")
+        print ("\n[+] Target: %s" % hostx.address)
+        # Querying HackerTarget.com API
 
-print ("|" + "-" * 65 + "|")
+        try:
+        #    http = urllib3.PoolManager()
+        #    r3 = http.request('GET', 'https://api.hackertarget.com/reverseiplookup/?q=%s' % ip,decode_content=True).read().decode("UTF-8")
+        #    print (r3)
+            r2 = urllib.request.urlopen('https://api.hackertarget.com/reverseiplookup/?q=%s' % ip).read().decode("UTF-8")
+            if (r2.find("No DNS A records found")==-1) and (r2.find("API count exceeded")==-1 and r2.find("error")==-1):
+                for host in r2.split('\n'):
+                    if (host=="") or (host in hostx.hname):
+                        pass
+                    else:
+                        hostx.hname.append(host)
+            else:
+                pass
+        except (urllib.error.HTTPError,socket.timeout) as e:
+            print ("[*] Error: connecting with HackerTarget.com API")
+
+        # Querying Bing.com
+        if args.bing == True:
+            try:
+                r3 = urllib.request.urlopen('https://www.bing.com/search?q=ip%%3a%s' % ip).read().decode("UTF-8")
+                bing_response = re.findall(pattern,r3)
+                bing_results = bing_response #bing_response[7:]
+                for item in bing_results:
+                    url = re.findall("(http(s)?://[^\s]+)",item)[0][0]
+                    item = re.sub("\"","",url)
+                    hostx.apps.append(url)
+                    host = re.sub("(http(s)?://)","",item)
+                    host2 = re.sub("/(.)*","",host)
+                #    print (host);
+                    if (host2=="") or (host2 in hostx.hname):
+                        pass
+                    else:
+                        hostx.hname.append(host2)
+            except (urllib.error.HTTPError,socket.timeout) as e:
+                print ("[*] Error: connecting with Bing.com")
+
+        # Fetch SSL Certificates
+        try:
+            cert=ssl.get_server_certificate((ip, 443))
+            x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+            cert_hostname=x509.get_subject().CN
+            # Capture Screenshots with -cs option
+            if args.screen_capture == True:
+                take_screenshot(ip,443)
+            # Add New HostNames to List
+            for host in cert_hostname.split('\n'):
+                if (host=="") or (host in hostx.hname):
+                    pass
+                else:
+                    hostx.hname.append(host)
+        except (requests.ConnectionError,requests.Timeout,socket.error,socket.timeout) as e:
+            pass
+
+        if hostx.hname:
+            print ("[+] Hostnames: ", end = "\n")
+            for item in hostx.hname:
+                print (item)
+                # Write output to .TXT file
+                if args.format.lower()=='txt':
+                    vhostsf.write(item + "\n")
+                counter += 1
+
+            # Write output to .CSV file
+            if args.format.lower() == 'csv':
+                hostnames = ','.join(hostx.hname) # Merging the lists prooved Faster than list iterations
+                row = "\"" + ip + "\"," + "\"443/tcp\"" + "," + "\"" + hostnames + "\",\"\",\"\",\"\"" + "\n"
+                vhostsf.write(row)
+
+                if args.bing:
+                    apps = ','.join(hostx.apps)
+                    row = "\"" + hostx.address + "\"," + "\"443/tcp\"" + "," + "\"" + apps + "\"" + "\n"
+                    appsf.write(row)
+
+        else:
+            print ("[-] Hostnames: no results ")
+            continue
+
+        if (args.bing == True):
+            print ("[+] Web Apps:")
+            for url in hostx.apps:
+                print (url)
+
+        # END FOR LOOP
+    # END IF
+    targets.close()
+
+    if args.screen_capture == True:
+        driver.quit()
+    # IPV6 https://[%ip6]
+    #  Robtex
+    #  https://freeapi.robtex.com/pdns/reverse/$ip
+
+    print ("\n" + "|" + "-" * 95 + "|", end = "\n\n")
+    print ("  Reconnaissance Completed!", end = "\n\n")
+    if counter==0:
+        print ("  0 hostname was discovered in %s sec" % (round(time.time() - start_time,2)), end = "\n\n")
+    else:
+        print ("  %s hostnames were discovered in %s sec" % (counter,round(time.time() - start_time,2)), end = "\n\n")
+    print ("|" + "-" * 95 + "|")
+#End of Main Function
+
+if __name__ == "__main__":
+    start_time = time.time() # Start Counter
+    display_banner() # Banner
+
+    if args.screen_capture == True:
+        driver = webdriver.Chrome(executable_path=DRIVER,chrome_options=chrome_opt)
+        driver.set_page_load_timeout(10)
+        if not os.path.exists(sc_path):
+            os.makedirs(sc_path)
+        print ("    Screenshots saved at: ",os.getcwd()+ "/" +sc_path)
+        print ("|" + "-" * 95 + "|", end = "\n\n")
+
+    main(sys.argv)
+#EOF
